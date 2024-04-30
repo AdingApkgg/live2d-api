dd269093126ae91aec9db2964bc59a0b705584ab
AdingApkgg <AdingApkgg@outlook.com>
2024-04-30 22:12:52 +0800

    .

--- js/autoload.js
+++ js/autoload.js
@@ -1,9 +1,4 @@
 // 注意：live2d_path 参数应使用绝对路径
-// const live2d_path =
-//   "https://fastly.jsdelivr.net/gh/nova1751/live2d-widget@latest/";
-
-// const live2d_path = "https://cdn.staticaly.com/gh/adingapkgg/live2d-api@main/";
-// const live2d_path = "https://jsd.onmicrosoft.cn/gh/adingapkgg/live2d-api@main/";
 const live2d_path = "https://registry.npmmirror.com/weblive2d-api/latest/files/";
 
 // 封装异步加载资源的方法
@@ -33,14 +28,13 @@ if (screen.width >= 768) {
     loadExternalResource(live2d_path + "js/live2d.min.js", "js"),
     loadExternalResource(live2d_path + "js/waifu-tips.js", "js"),
   ]).then(() => {
+
     // 配置选项的具体用法见 README.md
     initWidget({
       waifuPath: live2d_path + "waifu-tips.json",
 
       // apiPath: "https://live2d.fghrsh.net/api/",
-      // cdnPath: "https://live2d-api.saop.cc/",
       cdnPath: "https://registry.npmmirror.com/weblive2d-api/latest/files/",
-      // cdnPath: "/",
 
       tools: [
         "hitokoto",
