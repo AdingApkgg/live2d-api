1faf96ed4a09275a97579a7769af4c8777ecf048
AdingApkgg <AdingApkgg@outlook.com>
2024-05-08 19:09:50 +0800

    npm publish

--- js/autoload.js
+++ js/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 // const live2d_path = "https://cdn.jsdelivr.net/gh/adingapkgg/live2d-api/";
-const live2d_path = "https://registry.npmmirror.com/weblive2d/latest/files/";
+const live2d_path = "/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
